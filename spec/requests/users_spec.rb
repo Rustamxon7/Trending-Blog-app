f4d589938e6f5e5f302d53a6065c387399184277
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-08 17:31:13 +0500

    Check and fix linters errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /index" do
-    it "includes the user title index" do
-      get "/users/index"
-      expect(response.body).to include("User")
+  describe 'GET /index' do
+    it 'includes the user title index' do
+      get '/users/index'
+      expect(response.body).to include('User')
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "includes the user title show" do
-      get "/users/show"
-      expect(response.body).to include("User Title show")
+  describe 'GET /show' do
+    it 'includes the user title show' do
+      get '/users/show'
+      expect(response.body).to include('User Title show')
     end
   end
 end
