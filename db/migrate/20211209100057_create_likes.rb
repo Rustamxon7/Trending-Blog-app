13957f2423a2570e1dc3f0c43fc58aaba7e766a3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-09 15:05:44 +0500

    Create migration file for `likes`

---
+++ db/migrate/20211209100057_create_likes.rb
@@ -0,0 +1,10 @@
+class CreateLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :likes do |t|
+      t.text :text
+      t.timestamps
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
+    end
+  end
+end
