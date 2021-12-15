81253ec27f34b20e7c72bbcc4fbbaf84f452243e
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-16 01:07:00 +0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  # validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  # validates :text, presence: true, length: { minimum: 10, maximum: 1000 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(3)
+  end
+
+  def preview
+    text.split("\n").first(2).join("\n")
   end
 
   after_save :update_post_counter
