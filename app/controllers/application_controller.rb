5b0c57c7598632a4233138b6502bc1acd5fdd57a
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-16 01:07:29 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   def current_user
     User.first
   end
+
+  # show only one line of text for posts
+  def text
+    @post = Post.find(params[:id])
+    @text = @post.text.split("\n").first
+  end
 end
