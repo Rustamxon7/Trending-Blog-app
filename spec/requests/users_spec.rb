2c8eb00e1553d0a613cd6c4805fc8b994cc04cb3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-16 13:59:17 +0500

    Test users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,31 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+  describe 'validations' do
+    it 'validates the presence of the name' do
+      user = User.new
+      expect(user).to_not be_valid
+      expect(user.errors[:name]).to include("can't be blank")
     end
-  end
 
-  describe 'GET /index' do
-    it 'includes the user title index' do
-      get '/users/index'
-      expect(response.body).to include('User')
-    end
-  end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /show' do
-    it 'includes the user title show' do
-      get '/users/show'
-      expect(response.body).to include('User Title show')
+    it 'validates the posts_counter' do
+      user = User.new(posts_counter: -1)
+      expect(user).to_not be_valid
+      expect(user.errors[:posts_counter]).to include('must be greater than or equal to 0')
     end
   end
 end
