3d1fb9bb3959659e28571245845c96cb2c8d64fa
ahzia <ah.ziayosfi@gmail.com>
2021-12-24 13:12:16 +0430

    add some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @user = User.first
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def text
     @post = Post.find(params[:id])
     @text = @post.text.split("\n").first
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
   end
 end
