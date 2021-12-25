16db16d9a1c616706d1eafc1eac61f2fb134bce6
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-12-25 22:51:16 +0500

    Update old test files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,25 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
-    before do
-      @user = User.create(name: 'test', photo: 'test', bio: 'test')
-      @post = Post.create(title: 'test', text: 'test', author_id: @user.id)
-    end
-
-    it 'works! (now write some real specs)' do
-      get posts_path
-      expect(response).to have_http_status(200)
-    end
-
-    it 'should return a list of posts' do
-      get posts_path
-      expect(response.body).to include('posts-page')
-    end
-
-    it 'should return a list of posts' do
-      get posts_path
-      expect(response.body).to include('test')
-    end
+   describe 'GET /index' do
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -19,19 +19,4 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Add User')
     end
   end
-
-  describe 'POST /users' do
-    it 'should create a new user' do
-      post users_path, params: { user: { name: 'test', photo: 'test', bio: 'test' } }
-      expect(response).to redirect_to(user_path(User.last))
-    end
-  end
-
-  describe 'GET /users/:id' do
-    it 'should return a user' do
-      user = User.create(name: 'test', photo: 'test', bio: 'test')
-      get user_path(user)
-      expect(response.body).to include('test')
-    end
-  end
 end
